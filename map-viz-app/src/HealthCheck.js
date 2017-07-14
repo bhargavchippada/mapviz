08b1ec1e9b691a41c8a34728fe7c365670c8a0c6
Bhargav Chippada <bhargavchippada19@gmail.com>
2017-07-14 14:51:46 +0530

    Dockerfile and HealthCheck

---
+++ map-viz-app/src/HealthCheck.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class HealthCheck extends Component {
+    render() {
+        return (
+            <div>
+                <h3>SUCCESS</h3>
+            </div>
+        );
+    }
+}
+export default HealthCheck;
--- map-viz-app/src/index.js
+++ map-viz-app/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HealthCheck from './HealthCheck';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 require('bootstrap/dist/css/bootstrap.min.css');
 
 ReactDOM.render(
-    <App/>, document.getElementById('root'));
+    <Router>
+    <div>
+        <Route path="/health_check" component={HealthCheck}/>
+        <Route exact path="/" component={App}/>
+    </div>
+</Router>, document.getElementById('root'));
 registerServiceWorker();
